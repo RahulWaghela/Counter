701d3c0ee6a1cdb1957c58be94343a623ab04546
RahulWaghela <rahulwaghela191919@gmail.com>
2024-02-03 11:51:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react'
 function App() {
+  const [first, setfirst] = useState(1);
+  const increase=()=>{
+      setfirst(first+1);
+  }
+  const decrease=()=>{
+     if(first>=1){
+       setfirst(first-1);
+     }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>increase the value {first}</h1> 
+    <button onClick={decrease}>click to decrease</button> <button onClick={increase}>click to increase</button>  
+
+
+     <p>this is the sidebar{first}</p>
+     <p>this is the navbar{first}</p>
+     <p>this is the footer{first}</p>
+    </>
   );
 }
 
